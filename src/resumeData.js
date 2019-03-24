e3aef4ee5a7ecd03c3d744acb837db8107a58cc4
Bruno <brunopjsw@gmail.com>
2019-03-24 01:33:32 -0300

    first

---
+++ src/resumeData.js
@@ -0,0 +1,118 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Bruno Barbosa",
+    "role": "Desenvolvedor front-end",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Estudante de tecnologia da informação, focado na área de programação Web Client-side e Server-side",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/thedevjs/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/thedevb",
+          "className":"fa fa-github"
+        }
+      ],
+      "aboutme":" apaixonado por aprender e descobrir, autodidata sempre buscando criar novas sinapses! Tenho boa comunicação aprecio um bom meetup para fazer novas amizades. Gosto de trabalhar com web, atualmente front-end em uma startup lawtech.",
+    "address":"India",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"UMC - Mogi das Cruzes",
+        "specialization":"Sistema de informação",
+        "MonthOfPassing":"Fev",
+        "YearOfPassing":"2018",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"Programação web completo",
+        "specialization":"Curso web online",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Curso web de 108 horas com foco front-end na plataforma Udemy"
+      },
+      {
+        "UniversityName":"Aprenda a programar em um final de semana",
+        "specialization":"Curso presencial web",
+        "MonthOfPassing":"Dez",
+        "YearOfPassing":"2017",
+        "Achievements":"Curso de 16 horas oferecido pelo facebook na estação hack São Paulo"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Hackathon",
+        "specialization":"Hackathon da seguraça pública de Mogi das Cruzes",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Hackathon organizado pela prefeitura de Mogi das Cruzes visando buscar soluções para a cidade por meio da tecnologia onde eu tive a felicidade de ficar em segundo lugar"
+      },
+      {
+        "CompanyName":"Docket Brasil",
+        "specialization":"Estagiário Front-end",
+        "MonthOfLeaving":"Ago",
+        "YearOfLeaving":"2018",
+        "Achievements":"Atual local de trabalho onde desenvolvo e faço melhorias em uma plataforma financeira interna"
+      }
+    ],
+    "skillsDescription":"Front-end",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Jquery"
+      },
+      {
+        "skillname":"SASS"
+      },
+      {
+        "skillname":"Git"
+      },
+      {
+        "skillname":"Scrum"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Conversor moeda",
+        "description":"Um conversor de moeda criado para estudo em reactjs",
+        "imgurl":"./images/converosor.jpg",
+        "classe":"conversor"
+      },
+      {
+        "name":"Academia site",
+        "description":"Site de academia com sistema back-end de gerenciamento de contato (PHP)",
+        "imgurl":"./images/academia.jpg",
+        "classe":"academia"
+      },
+      {
+        "name":"Jogo mata mosquito",
+        "description":"Jogo criado para estudo",  
+        "imgurl":"./images/mosquito.jpg",
+        "classe":"mosquito"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
