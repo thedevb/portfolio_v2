62a212010241f413a7bfec231996b27651a2aabf
Bruno <brunopjsw@gmail.com>
2019-03-24 00:48:25 -0300

    ...

---
+++ build/precache-manifest.47bc860eacc6be6d7068813930bce2c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "717113e672f30b1cf2b6",
+    "url": "/static/js/main.717113e6.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "717113e672f30b1cf2b6",
+    "url": "/static/css/main.fa51f868.chunk.css"
+  },
+  {
+    "revision": "34df1a7f2fe85d96fcd7bf4e425a7891",
+    "url": "/index.html"
+  }
+];
